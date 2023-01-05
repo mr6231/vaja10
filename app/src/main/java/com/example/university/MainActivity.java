f5d9c2982522e52a082d04abce2a927b0a150237
mr6231 <117918828+mr6231@users.noreply.github.com>
2023-01-05 23:36:48 +0100

    first commit

---
+++ app/src/main/java/com/example/university/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.university;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RequestQueue requestQueue;
+    private TextView osebe;
+    private String url = "https://vremenkoo.azurewebsites.net/pridobipostaje";
+
+    public static final String EXTRA_MESSAGE = "com.example.universityapp.MESSAGE";
+
+    public void addStudentActivity (View view) {
+        Intent intent = new Intent(this,AddStudentActivity.class);
+        String message = "Dodaj studenta v seznam.";
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+        osebe = (TextView) findViewById(R.id.osebe);
+    }
+
+    public void prikaziOsebe(View view){
+        if (view != null){
+            JsonArrayRequest request = new JsonArrayRequest(url, jsonArrayListener, errorListener);
+            requestQueue.add(request);
+        }
+    }
+
+    private Response.Listener<JSONArray> jsonArrayListener = new Response.Listener<JSONArray>() {
+        @Override
+        public void onResponse(JSONArray response){
+            ArrayList<String> data = new ArrayList<>();
+            for (int i = 0; i < response.length(); i++){
+                try {
+                    JSONObject object =response.getJSONObject(i);
+                    String ime = object.getString("Name");
+                    String temp = object.getString("Temp");
+                    String hum = object.getString("Hum");
+
+                    data.add(ime + " " + temp + " " + hum);
+
+                } catch (JSONException e){
+                    e.printStackTrace();
+                    return;
+
+                }
+            }
+
+            osebe.setText("");
+
+
+            for (String row: data){
+                String currentText = osebe.getText().toString();
+                osebe.setText(currentText + "\n\n" + row);
+            }
+
+        }
+
+    };
+
+    private Response.ErrorListener errorListener = new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            Log.d("REST error", error.getMessage());
+        }
+    };
+
+
+}
